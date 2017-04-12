e8a1bb5a8a3757d4d1a341a383711d1721354606
zzj <buu2012victoria@163.com>
2017-04-12 09:38:28 +0800

    task score

--- ProcessBehavior.py
+++ ProcessBehavior.py
@@ -15,6 +15,7 @@ stu_ids=['stu1','stu2','stu3','stu4','stu5','stu6','stu7','stu8','stu9','stu10',
 task_libs = {'601.htm': ['601.json']}
 
 pb_type = dict()
+task_score = dict()
 
 class PB:
     def __init__(self, verb, obj_id, obj_cn, obj_type, timestamp):
@@ -29,7 +30,17 @@ class Student:
         self.id = id
         self.pbs = []
         self.serlized_pbs = ''
-        
+        self.task_score = dict()
+
+def get_score(json):
+    obj, specify_obj = get_former_latter(get_obj_id(json['object']['id']), '#')
+    score = json['result']['response'].get('score')  
+    if web_task_map.get(obj) != None:
+        if score != None:
+            task_score[obj] = score
+        else:
+            task_score[obj] = 'null'
+            
 def process_behavior(data):
     pbs = []
     for i in range(0,len(data)):
@@ -37,7 +48,10 @@ def process_behavior(data):
         if type == '':
             continue
         verb = Util.parse_verb_id(data[i]['verb']['id'])
-        obj_id = get_obj_id(data[i]['object']['id'])
+        obj = data[i]['object']['id']
+        if obj.__contains__('statistic'):
+            get_score(data[i])
+        obj_id = get_obj_id(obj)
         obj_cn = '“' + data[i]['object']['definition']['name']['zh_CN'] + '”'
         timestamp = data[i]['timestamp']
         
@@ -48,6 +62,11 @@ def process_behavior(data):
     #filter scored    
     filtered_pbs = filter_verbs(pbs)
     
+    specify_pbs = specify_obj(filtered_pbs)
+                
+    return specify_pbs
+
+def specify_obj(filtered_pbs):
     #differentiate lib, specify obj
     in_task = False
     task_id = ''
@@ -71,10 +90,15 @@ def process_behavior(data):
         elif (f_pb.obj_type == 'task') & (in_task == True) & (f_pb.verb != 'completed'):
             obj, specify_obj = get_former_latter(f_pb.obj_id, '#')
             if specify_obj != None:   
-                f_pb.obj_id = f_pb.obj_id + ' ' + specify_obj
+                if f_pb.verb == 'drag':
+                    if (specify_obj != 'women') & (specify_obj != 'man') & (specify_obj != 'boy') & (specify_obj != 'girl'):
+                        specify_obj = 'out'
+                    else:
+                        specify_obj = 'in'
+                        
+                f_pb.obj_id = obj + ' ' + specify_obj
                 f_pb.obj_cn = f_pb.obj_cn + ' ' + specify_obj
-                
-             
+
     return filtered_pbs
 
 def lib_usefulness(task_id, lib_id):
@@ -153,17 +177,27 @@ def pb_type_map(pbs):
         else:
             pb_type[key] = pb.verb + ' ' + pb.obj_cn
 
-def print_dict(dict):
-    file = open('process_behavior_type2.txt', 'w')
+def print_dict(file, dict, has_key, stu_id):
+    
+    if stu_id != None:
+        file.write(stu_id + '\n')
     for key, value in dict.items():
-        file.write(value + '\n')
-        print (value)
-    print(len(pb_type))  
-    file.close()
-           
+        
+        if has_key:
+            file.write(key + ':' + str(value) + '\n')
+            #print (key + ':' + str(value))
+        else:
+            file.write(str(value) + '\n')
+            #print (str(value))
+    #print(len(pb_type))  
+
+def open_file(file_name):
+    file = open(file_name, 'w')
+               
 def main():
     data = []
-    
+    task_score_file = open('task_score.txt', 'w')
+    pb_type_file = open('process_behavior_type2.txt', 'w')
     for id in stu_ids:
         stu = Student(id)
         stu.pbs = process_behavior(Util.data_read('20170328/' + id))
@@ -172,9 +206,12 @@ def main():
         stu_output = [stu.id, stu.serialized_pbs]
         data.append(stu_output)
         pb_type_map(stu.pbs)
-        
+        stu.task_score = task_score
+        print_dict(task_score_file, task_score, True, stu.id)
     d = pd.DataFrame(data = data, columns = get_title())
     d.to_csv('process_behavior2.csv')
-    print_dict(pb_type)
+    print_dict(pb_type_file, pb_type, False, None)
+    pb_type_file.close()
+    task_score_file.close()
         
 if __name__ == "__main__": main()
